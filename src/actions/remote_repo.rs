58b4b392ce653f548405a49488cf5928e1da6f20
Othi <mnpq.raven@gmail.com>
2024-10-29 14:13:55 +0700

    fix slug

--- src/actions/remote_repo.rs
+++ src/actions/remote_repo.rs
@@ -10,25 +10,28 @@ enum FsResolveAction {
 /// clones a service from github into path
 /// TODO: privatize
 pub fn clone_single(github_slug: &str) {
-    let out_dir = "/home/ubuntu/service_repos";
+    let out_dir = "./service_repos";
     // create if not exist
     // TODO: UNWRAP
     fs::create_dir_all(out_dir).unwrap();
+    let url = format!("https://github.com/{github_slug}");
+
     // git -C <path> clone <url>
     let _clone = Command::new("git")
-        .args(["-C", out_dir, "clone", github_slug])
+        .args(["-C", out_dir, "clone", &url])
         .output()
         .unwrap();
+    println!("stdout: {}", String::from_utf8_lossy(&_clone.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&_clone.stderr));
 }
 
 /// pull a service repo in path
 fn pull_single(name: &str) {
-    let out_dir = format!("/home/ubuntu/service_repos/{name}" );
+    let out_dir = format!("/home/ubuntu/service_repos/{name}");
     let _clone = Command::new("git")
         .args(["-C", &out_dir, "pull"])
         .output()
         .unwrap();
-
 }
 
 /// deploy/build the repo
